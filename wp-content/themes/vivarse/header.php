989a7f4f8308d1a63642d33c7b2d9ebf94dade9f
jurc192 <vidmarjur@gmail.com>
2017-08-18 01:02:09 +0200

    Added logo, todo: fix size & reponsive stuff

--- wp-content/themes/vivarse/header.php
+++ wp-content/themes/vivarse/header.php
@@ -26,6 +26,7 @@
 
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'vivarse' ); ?></button>
+			<a class="site-logo" href="./"><img src="wp-content/themes/vivarse/images/logo.png" alt="ArsViva logo"></a>
 			<?php
 				wp_nav_menu( array(
 					'theme_location' => 'menu-1',
