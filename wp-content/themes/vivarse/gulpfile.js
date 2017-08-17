e331e9dfd7b14bb4e12e32490b5210b0d59ddcdf
jurc192 <vidmarjur@gmail.com>
2017-08-17 12:24:25 +0200

    Automation with GULP now working
    Finished initializing gulp, wrote custom gulpfile.js,
    automatic sass-css compiling on save, and automatic browser reloading
    on save - nice!

--- wp-content/themes/vivarse/gulpfile.js
+++ wp-content/themes/vivarse/gulpfile.js
@@ -1,11 +1,30 @@
 // My first GULP config
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
-gulp.task('helloTask', function() {
-  console.log("Hello Jure the task giver!");
+
+// $ gulp sass -> compiles sass into ./style.css
+gulp.task('sass', function() {
+  return gulp.src('sass/style.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
 });
 
-gulp.task('sass', function() {
-  return gulp.src('sass');
+// refresh browser automatically
+gulp.task('browserSync', function() {
+  browserSync.init({
+    proxy: "localhost/ArsViva_DEV/",
+    notify: false
+  });
+});
+
+// $ gulp watch -> watches for changes, re-compiles on every save
+gulp.task('watch', ['browserSync', 'sass'], function() {
+  gulp.watch('sass/**/*.scss', ['sass']);
+  gulp.watch('sass/**/*.html', browserSync.reload);
+  gulp.watch('sass/**/*.js', browserSync.reload);
 });
