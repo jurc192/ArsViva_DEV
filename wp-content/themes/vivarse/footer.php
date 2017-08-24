455303917f5bcae215537cd8845cc44196c2b8cb
jurc192 <vidmarjur@gmail.com>
2017-08-24 20:00:23 +0200

    Added (impro) shadows & footer

--- wp-content/themes/vivarse/footer.php
+++ wp-content/themes/vivarse/footer.php
@@ -9,6 +9,7 @@
  ?>
 
     <footer id="colophon" class="site-footer section fp-auto-height">
+      <img src="<?php bloginfo('template_url'); ?>/images/noga.png" alt="footer background">
       <div class="site-info">
 
         <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'vivarse' ) ); ?>"><?php
