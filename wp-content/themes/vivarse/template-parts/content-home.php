78c99fcbec1a4facad4e0af7b944ca43c5e81f50
jurc192 <vidmarjur@gmail.com>
2017-08-24 18:18:00 +0200

    Added colors & fonts
    Messed with _typography.scss, _colors.scss, ...

--- wp-content/themes/vivarse/template-parts/content-home.php
+++ wp-content/themes/vivarse/template-parts/content-home.php
@@ -19,6 +19,7 @@
 
   		if ( 'post' === get_post_type() ) : ?>
   		<div class="entry-meta">
+        <p class="type">tip dogodka</p>
   			<?php vivarse_posted_on(); ?>
   		</div><!-- .entry-meta -->
   		<?php
