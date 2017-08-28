4a3543f5be54fb4f73bebba48eb9476acb4e30e7
jurc192 <vidmarjur@gmail.com>
2017-08-28 21:49:42 +0200

    Working on front-page metadata parsing
    WIP: events metadata (where, when) embeeding to template.
    Get_the_category() doesn't work on frist try. Of course :)

--- wp-content/themes/vivarse/front-page.php
+++ wp-content/themes/vivarse/front-page.php
@@ -4,18 +4,64 @@ get_header(); ?>
 	<main id="main" class="site-main">
 
 		<?php
+
 		/* Custom query za front page - upcoming events */
+		$today = strtotime( 'today' );
+
+		/* Poglej če ni event že mimo */
+		$my_meta_query = array(
+			'relation' => 'AND',
+			array(
+				'key' => 'event-date',
+				'value' => $today,
+				'compare' => '>=',
+				'type' => 'NUMERIC'
+			)
+		);
+
 		$myquery_args = array(
 			'post_type' => 'event',
 			'post_status' => 'publish',
+			'meta_key' => 'event-date',
+			'orderby' => 'meta_value_num',
+			'order' => 'ASC',
+			'meta_query' => $my_meta_query,
 		);
-		$myquery = new WP_Query($myquery_args);
 
-		if ( $myquery->have_posts() ) :
+		$my_event_query = new WP_Query($myquery_args);
+
+		if ( $my_event_query->have_posts() ) :
 			if ( is_front_page() ):
 
-				while( $myquery->have_posts() ) : $myquery->the_post();
-					get_template_part( 'template-parts/content', 'home' );
+				while( $my_event_query->have_posts() ) : $my_event_query->the_post();
+					get_template_part( 'template-parts/content', 'front-event' );
+				endwhile;
+
+			else:
+				echo "<h1 style='z-index: 15; position: absolute; top: 100px; left: 100px;'>NOT FIRST PAGE!</h1>";
+			endif;
+
+		else:
+			echo "<h1 style='z-index: 15; position: absolute; top: 200px; left: 100px;'>I DON'T HAVE ANY EVENTS</h1>";
+		endif;
+
+		wp_reset_postdata();
+
+
+		/* Custom query za front page - front page posts */
+		$my_post_query_args = array(
+			'post_type' => 'post',
+			'post_status' => 'publish',
+			'category_name' => 'front-page',
+		);
+
+		$my_post_query = new WP_Query($my_post_query_args);
+
+		if ( $my_post_query->have_posts() ) :
+			if ( is_front_page() ):
+
+				while( $my_post_query->have_posts() ) : $my_post_query->the_post();
+					get_template_part( 'template-parts/content', 'front-post' );
 				endwhile;
 
 			else:
@@ -27,5 +73,6 @@ get_header(); ?>
 		endif;
 
 		wp_reset_postdata();
+
 
 get_footer();
