b0c63ab37e28608cfabd434a914452bdbc414f85
primoz <primoz.prevc95@gmail.com>
2017-09-19 18:28:19 +0200

    change text-tile min-height to 600, added contact form 7

--- wp-content/themes/vivarse/page-contact.php
+++ wp-content/themes/vivarse/page-contact.php
@@ -1,37 +1,32 @@
 <?php get_header(); ?>
 
-	<main id="main" class="site-main">
-		<?php get_sidebar(); ?>
-		<article class="section">
-			<img class="backgr" src="<?php bloginfo('template_url'); ?>/images/kontakt.jpg" alt="fotka1">
 
-			<div class="text-tile">
-				<h2>ARS VIVA</h2>
+<main id="main" class="site-main">
+	<?php get_sidebar(); ?>
+	<article class="section">
+		<iframe class ="google-maps"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d125287.85042863067!2d14.320511176443311!3d45.70596760145985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4764cfaa2633ea05%3A0xbd1161b05b7f93b7!2sArs+Viva%2C+zavod+za+kulturno+integracijo+in+Socializacijo+dru%C5%BEbenih+skupin!5e1!3m2!1ssl!2ssi!4v1503920112206" allowfullscreen></iframe>
+		<div class="text-tile">
+			<div id="contact-logo-div">
+				<img id="contact-logo" src="<?php bloginfo('template_url'); ?>/images/logo.png">
+				<p> Zavod za kulturno <br> integracijo in socializacijo <br> kulturnih skupin </p>
+			</div>
+			<div id="contact-info-div">
 				<p>
-					ARS VIVA- Zavod za kulturno integracijo in socializacijo družbenih skupin <br>
-					Podcerkev 24, 1386 Stari trg pri Ložu <br>
-					TRR 03133-1000202343 SKB <br>
-					ID za DDV: SI39453600 <br>
-					tel: (386) (0)5 726 47 21 <br>
-					mob: (386) (0)41 741 750 <br>
-					mail: ars.viva@gmail.com
+				Podcerkev <br> 24 1386 Stari trg pri Ložu <br> Slovenija </p><p>
+				TRR 03133-1000202343 SKB <br>
+				ID za DDV: SI39453600  </p><p>
+				tel: (386) (0)5 726 47 21 <br>
+				mob: (386) (0)41 741 750 </p><p>
+				youthostel.arsviva@gmail.com
 				</p>
 			</div>
-		</article>
-
-		<article class="section">
-			<iframe class ="google-maps"  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d125287.85042863067!2d14.320511176443311!3d45.70596760145985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4764cfaa2633ea05%3A0xbd1161b05b7f93b7!2sArs+Viva%2C+zavod+za+kulturno+integracijo+in+Socializacijo+dru%C5%BEbenih+skupin!5e1!3m2!1ssl!2ssi!4v1503920112206" allowfullscreen></iframe>
-			<div class="text-tile-small">
-				<h2>Lokacija</h2>
-				<p>
-					Informacije o lokaciji. . .
-				</p>
+			<div id="formdiv">
+			<?php echo do_shortcode('[contact-form-7 id="51" title="Kontakt" html_class="use-floating-validation-tip"]');  ?>
 			</div>
-		</article>	
-
-		<div class="section">
-			<h1>FORMA</h1>
 		</div>
+	</article>
+
+
 
 <?php
 get_footer();
