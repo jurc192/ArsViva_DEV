b8841a6beb427463a769992ce06c9ff1aa0f96c0
jurc192 <vidmarjur@gmail.com>
2017-09-21 14:01:51 +0200

    Fixed both footer's CSS (works, but sketchy)

--- wp-content/themes/vivarse/footer-nofp.php
+++ wp-content/themes/vivarse/footer-nofp.php
@@ -14,11 +14,16 @@
     <img src="<?php bloginfo('template_url'); ?>/images/noga_z_logotipi.png" alt="footer background">
 
     <div class="site-info">
+      <h3>Zavod za kulturno integracijo<br>in socializacijo družbenih skupin</h3>
+      <p>Podcerkev 24 <br> 1386 Stari trg pri Ložu</p>
+      <p>TRR 03133-1000202343 SKB <br> ID za DDV: SI39453600</p>
+      <p>tel:(386)(0)5 726 47 21 <br> mob:(386)(0)41 741 750</p>
+    </div>
 
-    </div><!-- .site-info -->
+    <?php wp_footer(); ?>
   </footer><!-- #colophon .site footer -->
 
 </div><!-- #page .site -->
-<?php wp_footer(); ?>
+
 </body>
 </html>
--- wp-content/themes/vivarse/footer.php
+++ wp-content/themes/vivarse/footer.php
@@ -11,12 +11,10 @@
     <footer id="colophon" class="site-footer section fp-auto-height">
       <img src="<?php bloginfo('template_url'); ?>/images/noga_z_logotipi.png" alt="footer background">
       <div class="site-info">
-		<h3>Zavod za kulturno integracijo<br>in socializacijo družbenih skupin</h3>
-		<p>Podcerkev 24 <br> 1386 Stari trg pri Ložu</p>
-		<p>TRR 03133-1000202343 SKB <br> ID za DDV: SI39453600</p>
-		<p>tel:(386)(0)5 726 47 21 <br> mob:(386)(0)41 741 750</p>
-
-
+    		<h3>Zavod za kulturno integracijo<br>in socializacijo družbenih skupin</h3>
+    		<p>Podcerkev 24 <br> 1386 Stari trg pri Ložu</p>
+    		<p>TRR 03133-1000202343 SKB <br> ID za DDV: SI39453600</p>
+    		<p>tel:(386)(0)5 726 47 21 <br> mob:(386)(0)41 741 750</p>
       </div><!-- .site-info -->
     </footer><!-- #colophon .site footer 		 -->
 
