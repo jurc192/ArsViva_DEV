d0dc8955fd4efe80c4ff0eb3bdfebeebffa7965f
jurc192 <vidmarjur@gmail.com>
2017-09-26 16:20:05 +0200

    Fixed sidebar icons

--- wp-content/themes/vivarse/sidebar.php
+++ wp-content/themes/vivarse/sidebar.php
@@ -9,11 +9,12 @@
 ?>
 
 	<aside class="social-media-sidebar">
-		<div style="height: 35%;"></div>
 
-		<a href="https://www.facebook.com/ARS-VIVA-158123837631509/">  <img class="sidebar-icon" src="<?php bloginfo('template_url'); ?>/images/sidebar-fb.png"></a>
-		<a href="http://arsviva.znidarsic.net/"> <img class="sidebar-icon" src="<?php bloginfo('template_url'); ?>/images/sidebar-google.png"></a>
-		<a href="https://www.youtube.com/channel/UCqeegXsm_BkkVDH3mms_xdw"> <img class="sidebar-icon" src="<?php bloginfo('template_url'); ?>/images/sidebar-yt.png"></a>
+		<div class="sidebar-icon-wrapper">
+			<a href="https://www.facebook.com/ARS-VIVA-158123837631509/">  <img class="sidebar-icon" src="<?php bloginfo('template_url'); ?>/images/sidebar-fb.png"></a>
+			<a href="http://arsviva.znidarsic.net/"> <img class="sidebar-icon" src="<?php bloginfo('template_url'); ?>/images/sidebar-google.png"></a>
+			<a href="https://www.youtube.com/channel/UCqeegXsm_BkkVDH3mms_xdw"> <img class="sidebar-icon" src="<?php bloginfo('template_url'); ?>/images/sidebar-yt.png"></a>
+		</div>
 
 		<img onclick="toggleMode()" class="sidebar-icon" src="<?php bloginfo('template_url'); ?>/images/sidebar-google.png">
 	</aside>
